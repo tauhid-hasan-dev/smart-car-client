2a934536a53c7345e74354bc62536e72a59043f4
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-03 02:04:49 +0300

    delete action done

--- src/Pages/Orders/OrderRow.js
+++ src/Pages/Orders/OrderRow.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 const OrderRow = ({ order }) => {
-    const { serviceName, price, customer, phone, message, service } = order;
+    const { serviceName, price, customer, phone, message, service, _id } = order;
     const [orderService, setOrderService] = useState()
     console.log(order);
+
+    //we did this useEffect and fetched data from service collection and set to a state , because we need data(in this case img) to add info from service collection to add order page
 
     useEffect(() => {
         fetch(`https://smart-car-server.vercel.app/checkout/${service}`)
@@ -11,11 +13,26 @@ const OrderRow = ({ order }) => {
             .then(data => setOrderService(data))
     }, [service])
 
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are you sure to delete this order');
+        if (proceed) {
+            fetch(`https://smart-car-server.vercel.app/orders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+
+                })
+
+        }
+    }
+
     return (
         <tr>
             <th>
                 <label>
-                    <button className="btn btn-square btn-outline hover:bg-red-500 hover:border-red-500">
+                    <button onClick={() => handleDelete(_id)} className="btn btn-square btn-outline hover:bg-red-500 hover:border-red-500">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </button>
                 </label>
