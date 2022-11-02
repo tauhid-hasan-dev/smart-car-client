997eaecc9e4a0a9f1d8bd8abe856bf544d5a35b3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-02 19:29:22 +0300

    problem solved

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
     const { user } = useContext(AuthContext);
     const [orders, setOrders] = useState({});
-    
+
     //using query parameter to load specific user details
     useEffect(() => {
         fetch(`https://smart-car-server.vercel.app/orders?email=${user?.email}`)
@@ -13,7 +14,34 @@ const Orders = () => {
     }, [user?.email])
     return (
         <div>
-            <p>total orders : {orders.length}</p>
+            {/* <p>total orders : {orders?.length}</p> */}
+            {
+                !orders?.length && <p>loading..</p>
+            }
+            {orders?.length && <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow key={order._id} order={order}></OrderRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+            }
         </div>
     );
 };
