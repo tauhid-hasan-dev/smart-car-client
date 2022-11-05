dddaf3677aaa0e1af98d3dfdda37bb8bc078be02
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-05 11:13:02 +0300

    verfication done

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -33,6 +33,7 @@ const Checkout = () => {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('smart-token')}`
 
                 },
                 body: JSON.stringify(order)
